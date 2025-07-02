bf10a12a8fa6f6e9bb2697729bbca28b81c04c9f
kallouche abderraouf <kaabdo2004@gmail.com>
2025-07-02 17:51:01 +0100

    fix: removed unused imports and variables

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,6 @@
 import express, { RequestHandler } from 'express';
 import { refreshAccessToken, signin, signup } from "../controls/auth";
-import { upload } from '../../uploads/images';
+import { upload } from '../uploads/images';
 
 
 const authRouter=express.Router();
